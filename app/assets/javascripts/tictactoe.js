545644c4866b183d78036d862d0ec3bb054c1a74
Andrew Waller <andrewhwaller@gmail.com>
2019-09-26 17:32:58 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -5,11 +5,11 @@ const WINNING_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6],
 var currentGame = 0;
 var turn = 0;
 
+var player = () => turn % 2 ? 'O' : 'X';
+
 $(document).ready(function() {
   attachListeners();
 });
-
-var player = () => turn % 2 ? 'O' : 'X';
 
 function attachListeners() {
   $('td').on('click', function() {
